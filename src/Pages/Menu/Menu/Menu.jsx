0ddfe62c19733f226e6bf201d43415c50c71d490
dipankar-halder <dipankar15-11137@diu.edu.bd>
2024-09-18 01:07:56 +0600

    call us

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet-async';
+
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+      <h1>dado</h1>
+    </div>
+  );
+};
+
+export default Menu;
--- src/Pages/Shared/NavBar/Navbar.jsx
+++ src/Pages/Shared/NavBar/Navbar.jsx
@@ -64,7 +64,7 @@ const NavBar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-black"
             >
               {navOptions}
             </ul>
