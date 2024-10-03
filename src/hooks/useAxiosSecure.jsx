54a2cbdbc2e5998035b12b68c1040bf6caf98b95
dipankar-halder <dipankar15-11137@diu.edu.bd>
2024-10-03 17:37:24 +0600

    jwt

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
   baseURL: 'http://localhost:5000',
 });
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
   // request interceptor to add authorization header for every secure call to the api
   axiosSecure.interceptors.request.use(
     function (config) {
@@ -22,10 +26,15 @@ const useAxiosSecure = () => {
     function (response) {
       return response;
     },
-    error => {
+    async error => {
       console.log('status Error', error);
       const status = error.response.status;
-      return Promise.reject(status);
+      // for 401 or 403 logout the user and move the user to the login
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate('/login');
+      }
+      return Promise.reject(error);
     }
   );
 
